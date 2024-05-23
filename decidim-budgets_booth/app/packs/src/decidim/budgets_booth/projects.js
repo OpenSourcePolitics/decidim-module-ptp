4706d1abff04ca9e2f602ab3499aac9652944885
moustachu <moustachu@opensourcepolitics.eu>
2024-05-23 16:51:20 +0200

    fix: add JS compatibility with decidim-budget_category_voting module

--- decidim-budgets_booth/app/packs/src/decidim/budgets_booth/projects.js
+++ decidim-budgets_booth/app/packs/src/decidim/budgets_booth/projects.js
@@ -3,21 +3,51 @@ const initializeProjects = () => {
   const $budgetSummaryTotal = $(".budget-summary__total");
   const $budgetExceedModal = $("#budget-excess");
   const $budgetSummary = $(".budget-summary__progressbox");
+  const $voteButton = $(".budget-vote-button");
   const totalAllocation = parseInt($budgetSummaryTotal.attr("data-total-allocation"), 10);
 
   const cancelEvent = (event) => {
+    $(event.currentTarget).removeClass("loading-spinner");
     event.stopPropagation();
     event.preventDefault();
   };
+
+  $voteButton.on("click", "span", () => {
+    $(".budget-list__action").click();
+  });
+  
   $projects.on("click", ".customized-budget", (event) => {
     const currentAllocation = parseInt($budgetSummary.attr("data-current-allocation"), 10);
     const $currentTarget = $(event.currentTarget);
     const projectAllocation = parseInt($currentTarget.attr("data-allocation"), 10);
+
+    if (!$currentTarget.attr("data-open")) {
+      $currentTarget.addClass("loading-spinner");
+    }
+    
+    let $targetCategory = null;
+    const $targetCategoryId = $currentTarget.parents(".budget-list__data").first().data("categoryId");
+    if ($targetCategoryId !== undefined) {
+        $targetCategory = $(`.vote_by_category[data-category-id=${$targetCategoryId}]`).first();
+    }
+
+    let quotaExceeded = ((currentAllocation + projectAllocation) > totalAllocation);
+    let categoryQuotaExceeded = false;
+
+    if ($targetCategory && $targetCategory.length > 0) {
+        const currentCategoryAllocation = parseInt($targetCategory.attr("data-current-allocation"), 10);
+        const totalCategoryAllocation = parseInt($targetCategory.attr("data-allocation"), 10);
+        categoryQuotaExceeded = ((currentCategoryAllocation + projectAllocation) > totalCategoryAllocation || quotaExceeded);
+    }
+    
     if ($currentTarget.attr("disabled")) {
       cancelEvent(event);
-    } else if (($currentTarget.attr("data-add") === "true") && ((currentAllocation + projectAllocation) > totalAllocation)) {
-      $budgetExceedModal.foundation("toggle");
+    } else if (($currentTarget.attr("data-add") === "true") && categoryQuotaExceeded === true) {
+      $(`#budget-excess-${$targetCategoryId}`).foundation("toggle");
       cancelEvent(event);
+    } else if (($currentTarget.attr("data-add") === "true") && quotaExceeded === true) {
+        $budgetExceedModal.foundation("toggle");
+        cancelEvent(event);
     }
   });
 };
