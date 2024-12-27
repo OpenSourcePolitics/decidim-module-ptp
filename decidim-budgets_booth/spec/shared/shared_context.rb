c1c4efb090e0b36c7c86c06b2bd5ce8d27339e72
stephanie rousset <strabon@club-internet.fr>
2024-12-27 11:10:18 +0100

    fix: update shared_context

--- decidim-budgets_booth/spec/shared/shared_context.rb
+++ decidim-budgets_booth/spec/shared/shared_context.rb
@@ -3,34 +3,6 @@
 shared_context "with scopes" do
   let(:parent_scope) { create(:scope, organization: organization) }
   let!(:subscopes) { create_list(:scope, 3, parent: parent_scope, organization: organization) }
-  let!(:first_postals) do
-    [].tap do |postals|
-      5.times do |i|
-        code = (10_000 + i).to_s
-        postals << create(:scope, name: { en: code }, code: "FIRST_#{code}", parent: subscopes[0], organization: organization)
-      end
-    end
-  end
-  let!(:second_postals) do
-    [].tap do |postals|
-      7.times do |i|
-        code = (10_010 + i).to_s
-        postals << create(:scope, name: { en: code }, code: "SECOND_#{code}", parent: subscopes[1], organization: organization)
-      end
-    end
-  end
-  let!(:third_postals) do
-    [].tap do |postals|
-      8.times do |i|
-        code = (10_020 + i).to_s
-        postals << create(:scope, name: { en: code }, code: "THIRD_#{code}", parent: subscopes[2], organization: organization)
-      end
-    end
-  end
-end
-
-shared_context "with user data" do
-  let!(:user_data) { create(:user_data, component: component, user: user) }
 end
 
 shared_context "with scoped budgets" do
@@ -47,23 +19,9 @@ shared_context "with scoped budgets" do
 
   before do
     # We update the description to be less than the truncation limit. To test the truncation, we update those in tests.
-    attach_images(budgets)
     budgets[0].update!(scope: parent_scope, description: { en: "<p>Eius officiis expedita. 55</p>" })
     budgets[1].update!(scope: subscopes[0], description: { en: "<p>Eius officiis expedita. 56</p>" })
     budgets[2].update!(scope: subscopes[1])
-  end
-
-  private
-
-  def attach_images(budgets)
-    city_files = ["city.jpeg", "city2.jpeg", "city3.jpeg"]
-    budgets.each_with_index do |budget, ind|
-      budget.update(main_image: ActiveStorage::Blob.create_and_upload!(
-        io: File.open(Decidim::Dev.asset(city_files[ind])),
-        filename: city_files[ind],
-        content_type: "image/jpeg"
-      ))
-    end
   end
 end
 
@@ -79,16 +37,6 @@ shared_context "with single scoped budget" do
 
   before do
     budget.update!(scope: subscopes[0], description: { en: "<p>Eius officiis expedita. 55</p>" })
-  end
-end
-
-shared_context "with zip_code workflow" do
-  let!(:component) do
-    create(
-      :budgets_component,
-      settings: component_settings.merge(workflow: "zip_code"),
-      organization: organization
-    )
   end
 end
 
