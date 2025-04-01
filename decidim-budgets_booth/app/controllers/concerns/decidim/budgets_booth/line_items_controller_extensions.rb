3efd5bf05db5a23dd0340bb841d5bd292d61a8d6
stephanie rousset <strabon@club-internet.fr>
2025-04-01 16:20:40 +0200

    feat: update set_help_modal method to take into account minimum vote project setting

--- decidim-budgets_booth/app/controllers/concerns/decidim/budgets_booth/line_items_controller_extensions.rb
+++ decidim-budgets_booth/app/controllers/concerns/decidim/budgets_booth/line_items_controller_extensions.rb
@@ -14,9 +14,14 @@ module Decidim
         private
 
         def set_help_modal
+          minimum_vote = current_component[:settings]["global"]["vote_selected_projects_minimum"]
+          line_items = Decidim::Budgets::Order.find_by(user: current_user, budget: budget)&.line_items
           @show_help_modal =
             if current_workflow.try(:disable_voting_instructions?)
               false
+            elsif minimum_vote && line_items && minimum_vote == line_items.size + 1
+              # when we click to add a project, it has not yet been added in line items so we add + 1 to get the modal at the right time
+              true
             else
               Decidim::Budgets::Order.find_by(user: current_user, budget: budget).blank?
             end
