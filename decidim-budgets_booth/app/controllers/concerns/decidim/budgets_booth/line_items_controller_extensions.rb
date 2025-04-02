b86e48031e5aac1be2d348c6f1383e8defeccf0f
stephanie rousset <strabon@club-internet.fr>
2025-04-02 17:41:09 +0200

    refactor: set_help_modal method

--- decidim-budgets_booth/app/controllers/concerns/decidim/budgets_booth/line_items_controller_extensions.rb
+++ decidim-budgets_booth/app/controllers/concerns/decidim/budgets_booth/line_items_controller_extensions.rb
@@ -16,7 +16,8 @@ module Decidim
         def set_help_modal
           rule_enabled = current_component[:settings]["global"]["vote_rule_selected_projects_enabled"]
           minimum_vote = current_component[:settings]["global"]["vote_selected_projects_minimum"]
-          line_items = Decidim::Budgets::Order.find_by(user: current_user, budget: budget)&.line_items
+          order ||= Decidim::Budgets::Order.find_by(user: current_user, budget: budget)
+          line_items = order&.line_items
 
           @show_help_modal =
             if current_workflow.try(:disable_voting_instructions?)
@@ -25,7 +26,7 @@ module Decidim
               # when we click to add a project, it has not yet been added in line items so we add + 1 to get the modal at the right time
               true
             else
-              Decidim::Budgets::Order.find_by(user: current_user, budget: budget).blank?
+              order.blank?
             end
         end
       end
